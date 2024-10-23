5d791032df4bbab38413b780200b96a0ed39c724
Gargi1820 <86142460+Gargi1820@users.noreply.github.com>
2024-10-23 23:30:54 +0530

    Create CATEGORIES.sql

---
+++ CATEGORIES.sql
@@ -0,0 +1,9 @@
+REM INSERTING into CATEGORIES
+SET DEFINE OFF;
+Insert into CATEGORIES (CATEGORY_ID,CATEGORY_NAME) values (1,'Fiction');
+Insert into CATEGORIES (CATEGORY_ID,CATEGORY_NAME) values (2,'Non-Fiction');
+Insert into CATEGORIES (CATEGORY_ID,CATEGORY_NAME) values (3,'Science');
+Insert into CATEGORIES (CATEGORY_ID,CATEGORY_NAME) values (4,'History');
+Insert into CATEGORIES (CATEGORY_ID,CATEGORY_NAME) values (5,'Biography');
+Insert into CATEGORIES (CATEGORY_ID,CATEGORY_NAME) values (6,'Mystery');
+Insert into CATEGORIES (CATEGORY_ID,CATEGORY_NAME) values (7,'Romance');
